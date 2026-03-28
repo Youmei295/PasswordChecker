b0aa211327c2e4413183b293d34f82b7919cf3bc
Youmei <trantanphat.stu@gmail.com>
2026-03-28 14:34:12 +0700

    connect to database

--- main.py
+++ main.py
@@ -3,10 +3,53 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from checker import password_analyzer
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+import os
+import pyodbc
 
+def get_connection():
+    try:
+        conn_str = (
+            "DRIVER={ODBC Driver 18 for SQL Server};"
+            f"SERVER={os.getenv('DB_SERVER')};"
+            f"DATABASE={os.getenv('DB_NAME')};"
+            f"UID={os.getenv('DB_USER')};"
+            f"PWD={os.getenv('DB_PASSWORD')};"
+            "Encrypt=yes;"
+            "TrustServerCertificate=no;"
+            "Connection Timeout=30;"
+        )
+        return pyodbc.connect(conn_str)
+    except Exception as e:
+        print("Connection error:", e)
+        raise
 
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
 
-app = FastAPI()
+    cursor.execute("""
+    IF NOT EXISTS (
+        SELECT * FROM sysobjects 
+        WHERE name='results' AND xtype='U'
+    )
+    CREATE TABLE results (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        password NVARCHAR(255),
+        score NVARCHAR(50),
+        entropy FLOAT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+@asynccontextmanager
+
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,4 +64,17 @@ class PasswordRequest(BaseModel):
 
 @app.post("/analyze")
 def analyze(data: PasswordRequest):
-    return password_analyzer(data.password)
+    result = password_analyzer(data.password)
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO results (password, score, entropy) VALUES (?, ?, ?)",
+        data.password, result['score'], result['entropy']
+    )
+
+    conn.commit()
+    conn.close()
+
+    return result
